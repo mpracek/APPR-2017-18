6c8d4631a3c6f950b50a2b1a8e83a0f963d9c3d7
Martin Praček <martin.pracek@student.fmf.uni-lj.si>
2017-11-30 09:50:54 +0100

    odstranil sumnike iz imen virov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -19,42 +19,45 @@ bele.sorte <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']")
 #velikosti vinogradov od 2000 do 2016
 velikost_slovenskih_vinogradov = read.csv2("podatki/velikost_vinogradov.csv")
 #velikosti vinogradov in število trt 2000 in 2010
-velikost.stevilo_trt =read.csv2("podatki/velikost_vinogradov_število_trt.csv")
+velikost.stevilo_trt =read.csv2("podatki/velikost_vinogradov_trte.csv")
 #pridelovalci po statističnih regijah
-pridelovalci_regije = read.csv2(
-  "podatki/Pridelovalci, površina, število vinogradov in število sadik glede na sedež kmetijskega gospodarstva, statistične regije, Slovenija, 2009 in 2015")
+pridelovalci_regije = read.csv2("podatki/pridelovalci")
+#Pridelovalci po površini
+pridelovalci_povrsina = read.csv2("podatki/sorte_starost")
 #pridelovalci po površini
-pridelovalci_povrsina =read.csv2("podatki/razredi_pridelovalcev_2009_2015.csv")
+pridelovalci_povrsina =read.csv2("podatki/razredi_pridelovalcev.csv")
 #površina in število vinogradov glede na nagib vertikal po vinorodnih deželah in okoliših
-vertikale = read.csv2("podatki/Povšina_nagib_vertikal.csv")
+vertikale = read.csv2("podatki/nagib_vertikal.csv")
+#Nagib vinogradov
+nagib = read.csv2("podatki/nagib")
 #površina, število vinogradov in sadik glede na zatravljenost
-zatravljenost = read.csv2("podatki/Površina_zatravljenost.csv")
+zatravljenost = read.csv2("podatki/zatravljenost.csv")
 #površina, število vinogradov in sadik 2009, 2015
-povrsina.stevilo_vinogradov.sadik = read.csv2("
-  podatki/Površina, število vinogradov in število sadik po VINORODNA DEŽELA IN OKOLIŠ, VELIKOSTNI RAZREDI, LETO , MERITVE.csv")
+povrsina.stevilo_vinogradov.sadik = read.csv2("podatki/povrsina,stevilo.vinograd,sadik.csv")
 #podlaga na kateri rastejo trte 2009 in 2015
-podlaga = read.csv2("podatki/Površina_podlaga_2009_2015")
+podlaga = read.csv2("podatki/podlaga")
 #način gojenja trt
-gojenje = read.csv2("podatki/Površina_gojitvena_oblika_2009_2015")
-#ekološka pridelava (primerjava)
-ekolosko = read.csv2("podatki/Ekološka_pridelava.csv")
-#površina in število trsnic, matičnjakov in vinogradov (primerjava)
-trsnice.maticnjaki = read.csv2("podatki/trsnice_matičnjaki_vinogradi_regije_2009.csv")
+gojenje = read.csv2("podatki/gojitvena_oblika")
+
 
 #Drugi del
 
 #Najpogostejše sorte po površini, številu sadik
-sorte.povrsina_sadike = read.csv2("
-                                 podatki/Povšina vinogradov in število sadik najpogostejših sort, vinorodne dežele, Slovenija, 2009 in 2015.csv")
+sorte.povrsina_sadike = read.csv2("podatki/povrsina,sadik-sorte.csv")
 #Starost trt po površini in številu sadik
-starost.povrsin_sadike = read.csv2("podatki/Površina vinogradov in število sadik po starostnih razredih, vinorodne dežele in okoliši, Slovenija, 2009 in 2015")
+starost.povrsina_sadike = read.csv2("podatki/povrsina,sadike-starost")
 #Najpogostejše sorte po starosti
-sorte_starost = read.csv2("podatki/Površina_najpogostejše_sorte_starost_2009_2015.csv")
+sorte_starost = read.csv2("podatki/sorte_starost.csv")
 #Pridelava grozdja
-grozdje = read.csv2("podatki/pridelava_grozdja2000-2016.csv")
+grozdje = read.csv2("podatki/pridelava_grozdja.csv")
 #Pridelava po količini 2009
 kolicina = read.csv2("podatki/pridelovalci_količina.csv")
                                            
+#Primerjalni del
+#ekološka pridelava (primerjava)
+ekolosko = read.csv2("podatki/ekoloska_pridelava.csv")
+#površina in število trsnic, matičnjakov in vinogradov (primerjava)
+trsnice.maticnjaki = read.csv2("podatki/trsnice.csv")
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
