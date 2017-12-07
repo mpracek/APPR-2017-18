88aef0bae112489ee77a672e420a4dd8585c623e
Martin Praček <martin.pracek@student.fmf.uni-lj.si>
2017-12-07 09:53:41 +0100

    zamenjal locale

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,7 +1,5 @@
 # 2. faza: Uvoz podatkov
 
-sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
-
 # Funkcija, ki uvozi podatke o vinskih sortah iz Wikipedije
 # Vire sem prenesel 30.11.2017 
 link <- "https://en.wikipedia.org/wiki/List_of_grape_varieties"
