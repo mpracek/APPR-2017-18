208ccaea699c403b6219df3cb9d0c5d307cd6b2a
mpracek <mpracek299@gmail.com>
2017-12-21 22:46:42 +0100

    Prvi grafi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,21 +1,137 @@
 # 3. faza: Vizualizacija podatkov
 
-eko_sprememba <- ggplot(ekolosko) + aes(x = Leto, y = Vinogradi) + geom_line()
-print(eko_sprememba)
+##
+#Prvi del
+##
 
-
+#Nagibi
 nagibi <- stidy_nagib$Vinorodna.dezela 
-nagibi_slika_povrsina <- ggplot(stidy_nagib) + aes(x = Leto,
-                                                   y = Povrsina) +
-                                             geom_point()
+nagibi_slika_povrsina <- ggplot(stidy_nagib) + aes(x = Povrsina,
+                                                   y = Nagib) +
+                                             geom_line()
 print(nagibi_slika_povrsina)
 
+#Gojenje
+gojenje_slika_povrsina <- ggplot(stidy_gojenje) + aes(x = Povrsina,
+                                                      y = Nacin.gojenja) +
+                                                geom_line()
+print(gojenje_slika_povrsina)
+
+gojenje_slika_stevilo.sadik <- ggplot(stidy_gojenje) + aes(x = Stevilo.sadik,
+                                                      y = Nacin.gojenja) +
+  geom_line()
+print(gojenje_slika_stevilo.sadik)
+
+#Podlaga
+podlaga_slika_povrsina <- ggplot(stidy_podlaga) + aes(x = Povrsina,
+                                                      y = Nacin.gojenja) +
+  geom_line()
+print(podlaga_slika_povrsina)
+
+podlaga_slika_stevilo.sadik <- ggplot(stidy_podlaga) + aes(x = Stevilo.sadik,
+                                                           y = Nacin.gojenja) +
+  geom_line()
+print(podlaga_slika_stevilo.sadik)
+
+#Zatravljenost
+
+zatravljenost_slika_povrsina <- ggplot(stidy_zatravljenost) + 
+                                aes(x = Povrsina,
+                                    y = Zatravljenost) +
+                                geom_line()
+print(zatravljenost_slika_povrsina)
+
+zatravljenost_slika_stevilo.sadik <- ggplot(stidy_zatravljenost) + 
+                                     aes(x = Stevilo.sadik,
+                                         y = Zatravljenost) +
+                                     geom_line()
+print(zatravljenost_slika_stevilo.sadik)
+
+zatravljenost_slika_stevilo.sadik <- ggplot(stidy_zatravljenost) + 
+                                     aes(x = Stevilo.vinogradov,
+                                         y = Zatravljenost) +
+                                     geom_line()
+print(zatravljenost_slika_stevilo.sadik)
+
+##
+#Drugi del
+##
+
+# Površina in sadike po sortah
+
+sorte_slika_povrsina <- ggplot(stidy_sorte.povrsina_sadike) + 
+  aes(x = Povrsina,
+      y = Sorta) +
+  geom_line()
+print(sorte_slika_povrsina)
+
+sorte_slika_stevilo.sadik <- ggplot(stidy_sorte.povrsina_sadike) + 
+  aes(x = Stevilo.sadik,
+      y = Sorta) +
+  geom_line()
+print(sorte_slika_stevilo.sadik)
+
+#Sorte po starosti, površini in sadikah
+starost_slika_povrsina <- ggplot(stidy_starost.povrsina_sadike) + 
+  aes(x = Povrsina,
+      y = Starost) +
+  geom_line()
+print(starost_slika_povrsina)
+
+starost_slika_stevilo.sadik <- ggplot(stidy_starost.povrsina_sadike) + 
+  aes(x = Stevilo.sadik,
+      y = Starost) +
+  geom_line()
+print(starost_slika_stevilo.sadik)
+
+#Število vinogradov sadik
+stevilo_slika_povrsina <- ggplot(stidy_stevilo_vinogradov.sadik) + 
+  aes(x = Povrsina,
+      y = Velikostni.razred) +
+  geom_line()
+print(stevilo_slika_povrsina)
+
+stevilo_slika_stevilo.sadik <- ggplot(stidy_stevilo_vinogradov.sadik) + 
+  aes(x = Stevilo.sadik,
+      y = Velikostni.razred) +
+  geom_line()
+print(stevilo_slika_stevilo.sadik)
+
+stevilo_slika_stevilo.sadik <- ggplot(stidy_stevilo_vinogradov.sadik) + 
+  aes(x = Stevilo.vinogradov,
+      y = Velikostni.razred) +
+  geom_line()
+print(stevilo_slika_stevilo.sadik)
+
+##
+#Dodatna analiza
+##
+
+#ekološka predelava
+eko_sprememba <- ggplot(ekolosko) + aes(x = Leto, y = Vinogradi) + geom_path()
+print(eko_sprememba)
+
 graf <- ggplot(ekolosko) +
-                aes(x = Leto, 
-                    y = Vinogradi, 
-                    color = Meritev) +
-                    geom_point()
+  aes(x = Leto, 
+      y = Vinogradi, 
+      color = Meritev) +
+  geom_point()
 print(graf)
+
+#Matičnjaki
+
+Povrsina_maticnjaki <- ggplot(stidy_trsnice.maticnjaki) + 
+  aes(x = Povrsina,
+      y = Pridelava) +
+  geom_line()
+print(Povrsina_maticnjaki)
+
+stevilo_maticnjaki_nasadi <- ggplot(stidy_trsnice.maticnjaki) + 
+  aes(x = Stevilo.nasadov,
+      y = Pridelava) +
+  geom_line()
+print(stevilo_maticnjaki_nasadi)
+
 
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip",
