50a013b1a5248175b9429ddfdb89dd8851b35c9a
mpracek <mpracek299@gmail.com>
2018-01-18 14:08:58 +0100

    pozabi

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -327,7 +327,9 @@ sorte <- rbind(bele.sorte, rdece.sorte)
 # 
 # drzave <- sorte$'Drzava.izvora'
 #  
-# #veckratnik <- grep()# dvojni <- grep(("("), sorte$Drzava.izvora)
+# #veckratnik <- grep()
+
+# dvojni <- grep(("("), sorte$Drzava.izvora)
 # sorte1 <- apply(sorte[dvojni, ], 1, . %>% as.list() %>%
 # {data.frame(Ime = .$Ime,
 #              Drzava.izvora = .$Drzava.izvora %>%
