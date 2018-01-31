63f02c3510926702138bc185f0248c0ca2abb498
mpracek <mpracek299@gmail.com>
2018-01-31 20:40:27 +0100

    zacetno delo na shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -6,15 +6,17 @@ shinyUI(fluidPage(
   
   tabsetPanel(
     tabPanel("Države in avtohtone trtne sorte ",
-             DT::dataTableOutput("druzine")),
-    
+             selectInput(inputId = "variable",
+                         label = "Izberi državo iz seznama",
+                         choices = stevilo_sort$drzava.izvora, 
+                         selected = "Slovenia"))),
+             #print(zemljevid.drzave))
+    tabsetPanel(
       tabPanel("Slovensko vinogradništvo",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Ekološko vinogradništvo",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
+               sliderInput(inputId = "num", label = "Izberi sorto",
+                           min = 0, max = 100, value = 50)
+               )),
+    tabsetPanel(
+      tabPanel("Ekološko vinogradništvo")
     )
 ))
