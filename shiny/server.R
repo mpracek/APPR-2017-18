c276223df038b66242046a6a03c0eaea3cd5dfef
mpracek <mpracek299@gmail.com>
2018-02-14 00:06:53 +0100

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -12,5 +12,7 @@ shinyServer(
   output$eko.eko <- renderPlot({
     print(graf_napoved.eko)
   } )
+  output$tekst.pre <- renderText({napoved.pre.eko$st.vinogradov[napoved.pre.eko$Leto == "eko.preu"]})
+  output$tekst.eko <- renderText({napoved$st.vinogradov[napoved$Leto == "eko.leto"]})
   output$sorte <- DT::renderDataTable(datatable(stidy_sorte.povrsina_sadike))
 })
