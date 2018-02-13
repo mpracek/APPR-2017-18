dd1fbe2ea0d20ceaf90346f8d1996e74bfc84f4e
mpracek <mpracek299@gmail.com>
2018-02-13 23:37:52 +0100

    Update ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,21 +1,28 @@
 library(shiny)
 
-shinyUI(fluidPage(
+shinyUI(fluidPage(theme = shinytheme("superhero"),
   
   titlePanel("Slovensko vinogradništvo"),
   
   tabsetPanel(
     tabPanel("Države in avtohtone trtne sorte ",
-             selectInput(inputId = "drzave",
+             sidebarLayout(
+               sidebarPanel(
+                selectInput(inputId = "drzava",
                          label = "Izberi državo iz seznama",
                          choices = stevilo_sort$drzava.izvora, 
-                         selected = "Slovenia"))),
-      tabPanel("Slovensko vinogradništvo",
-               sliderInput(inputId = "num", label = "Izberi sorto",
-                           min = 0, max = 100, value = 50))
-))
-  #                )),
-#       tabPanel("Ekološko vinogradništvo",
-#               sliderInput(inputId = "num", label = "Izberi sorto",
-#               min = 0, max = 100, value = 50))
-# )
+                         selected = "Slovenia")),
+               mainPanel(textOutput("drzave"))),
+    tabsetPanel("Ekološko vinogradništvo"),
+              tabPanel("Ekološke kmetije v preusmeritvi",
+              sliderInput(inputId = "eko.preu", 
+                          label = "Izberi leto",
+                          min = 2017,max = 2025, value = 2020),
+              plotOutput("eko.pre")),
+              tabPanel("Vinogradi na ekoloških kmetijah",
+              sliderInput(inputId = "eko.leto", 
+                          label = "Izberi leto",
+                          min = 2017,max = 2025, value = 2020),
+              plotOutput("eko.eko")),
+    tabPanel("Slovensko vinogradništvo",
+              fluidRow(DT::dataTableOutput("tabela")))))))
