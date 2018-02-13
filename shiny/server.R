6f63e16d94a1e62a5e7f490b845d851ddf227cc9
mpracek <mpracek299@gmail.com>
2018-02-13 23:37:32 +0100

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -1,12 +1,16 @@
 library(shiny)
-shinyServer(function(input, output) {
-  
-  output$sorte <- renderText({
-    stevilo_sort %>% filter(drzava == input$select) %>%
-      transmute(text = paste0( drzava, "ima" ,stevilo, "avtohtonih sort." ) %>% as.character()
-  )
-  
-  output$slovenija <- renderPlot({
+shinyServer(
+  function(input, output) {
+  stevilo <- 
+  output$drzave <- renderText({paste0(input$drzava ,"ima",stevilo,
+                                      "avtohtonih sort.")
     
   })
+  output$eko.pre <- renderPlot({
+    print(graf.napovedi.preu)
+  })
+  output$eko.eko <- renderPlot({
+    print(graf_napoved.eko)
+  } )
+  output$sorte <- DT::renderDataTable(datatable(stidy_sorte.povrsina_sadike))
 })
