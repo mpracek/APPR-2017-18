e1054c3ebb9fdf2e51a848c1f6b27926ad18b229
mpracek <mpracek299@gmail.com>
2018-02-14 22:45:35 +0100

    Update ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,30 +1,28 @@
 library(shiny)
 
 shinyUI(fluidPage(theme = shinytheme("superhero"),
-  
-  titlePanel("Slovensko vinogradništvo"),
-  
-  tabsetPanel(
-    tabPanel("Države in avtohtone trtne sorte ",
-             sidebarLayout(
-               sidebarPanel(
-                selectInput(inputId = "drzava",
-                         label = "Izberi državo iz seznama",
-                         choices = stevilo_sort$drzava.izvora, 
-                         selected = "Slovenia")),
-               mainPanel(textOutput("drzave")), plotOutput("zemljevid")),
-    tabsetPanel("Ekološko vinogradništvo"),
-              tabPanel("Ekološke kmetije v preusmeritvi",
-              sliderInput(inputId = "eko.preu", 
-                          label = "Izberi leto",
-                          min = 2017,max = 2025, value = 2020),
-              plotOutput("eko.pre"),
-              textOutput("tekst.pre")),
-              tabPanel("Vinogradi na ekoloških kmetijah",
-              sliderInput(inputId = "eko.leto", 
-                          label = "Izberi leto",
-                          min = 2017,max = 2025, value = 2020),
-              plotOutput("eko.eko"),
-              textOutput("tekst.eko")),
-    tabPanel("Slovensko vinogradništvo",
-              fluidRow(DT::dataTableOutput("tabela")))))))
+                  titlePanel("Slovensko vinogradništvo"),
+                  tabsetPanel(
+                        tabPanel("Države in avtohtone trtne sorte ",
+                             sidebarLayout(
+                               sidebarPanel(
+                                 selectInput(inputId = "drzava",
+                                             label = "Izberi državo iz seznama",
+                                             choices = stevilo_sort$drzava.izvora, 
+                                             selected = "Slovenia")),
+                               mainPanel(textOutput("drzave"), plotOutput("zemljevid")))),
+                  tabsetPanel("Ekološko vinogradništvo"),
+                        tabPanel("Ekološke kmetije v preusmeritvi",
+                                      sliderInput(inputId = "eko.preu", 
+                                                  label = "Izberi leto",
+                                                  min = 2017,max = 2025, value = 2020),
+                                      plotOutput("eko.pre"),
+                                      textOutput("tekst.pre")),
+                         tabPanel("Vinogradi na ekoloških kmetijah",
+                                      sliderInput(inputId = "eko.leto", 
+                                                  label = "Izberi leto",
+                                                  min = 2017,max = 2025, value = 2020),
+                                      plotOutput("eko.eko"),
+                                      textOutput("tekst.eko")),
+                    tabsetPanel("Slovensko vinogradništvo",
+                                      fluidRow(DT::dataTableOutput("tabela"))))))
