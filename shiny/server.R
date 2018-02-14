887e6a294a7f03828e6117da93cf9ede9af6cc66
mpracek <mpracek299@gmail.com>
2018-02-14 22:45:06 +0100

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -1,19 +1,18 @@
 library(shiny)
 shinyServer(
   function(input, output) {
-  stevilo <- stevilo_sort$n[stevilo_sort$drzava.izvora == input$drzava]
-  output$drzave <- renderText({paste0(input$drzava ," ima ",
-                                      stevilo_sort$n[stevilo_sort$drzava.izvora == input$drzava],
-                                      " avtohtonih sort.")
-  output$zemljevid <- renderPlot({print(zemljevid.drzave)})  
+    output$drzave <- renderText({paste0(input$drzava ," ima ",
+                                        filter(stevilo_sort,drzava.izvora == input$drzava) %>% select(n),
+                                        " avtohtonih sort.")
+      })
+    output$zemljevid <- renderPlot({print(zemljevid.drzave)})  
+    output$eko.pre <- renderPlot({
+      print(graf.napovedi.preu)
+    })
+    output$eko.eko <- renderPlot({
+      print(graf_napoved.eko)
+    } )
+    output$tekst.pre <- renderText({napoved.pre.eko$st.vinogradov[napoved.pre.eko$Leto == "eko.preu"]})
+    output$tekst.eko <- renderText({napoved$st.vinogradov[napoved$Leto == "eko.leto"]})
+    output$sorte <- DT::renderDataTable(datatable(stidy_sorte.povrsina_sadike))
   })
-  output$eko.pre <- renderPlot({
-    print(graf.napovedi.preu)
-  })
-  output$eko.eko <- renderPlot({
-    print(graf_napoved.eko)
-  } )
-  output$tekst.pre <- renderText({napoved.pre.eko$st.vinogradov[napoved.pre.eko$Leto == "eko.preu"]})
-  output$tekst.eko <- renderText({napoved$st.vinogradov[napoved$Leto == "eko.leto"]})
-  output$sorte <- DT::renderDataTable(datatable(stidy_sorte.povrsina_sadike))
-})
