02194e6eac65bee6ad590f9dbce75832b3b609ff
mpracek <mpracek299@gmail.com>
2018-02-15 08:08:14 +0100

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -3,11 +3,9 @@ shinyServer(
   function(input, output) {
     output$drzave <- renderText({paste0(input$drzava ," ima ",
                                         filter(stevilo_sort,drzava.izvora == input$drzava) %>% select(n),
-                                        " avtohtonih sort.")
-      })
+                                        " avtohtonih sort.")})
     output$zemljevid <- renderPlot({print(zemljevid.drzave)})  
-    output$eko.pre <- renderPlot({
-      print(graf.napovedi.preu)})
+    output$eko.pre <- renderPlot({print(graf.napovedi.preu)})
     output$eko.eko <- renderPlot({print(graf_napoved.eko)} )
     outuput$slika1 <- renderImage({"slike/eko.vinograd.jpg"})
     output$tekst.pre <- renderText({napoved.pre.eko$st.vinogradov[napoved.pre.eko$Leto == "eko.preu"]})
