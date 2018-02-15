7a4776436c77d45a5f037ee29c0bcdf17a69c766
mpracek <mpracek299@gmail.com>
2018-02-15 08:04:34 +0100

    Update server.R

--- shiny/server.R
+++ shiny/server.R
@@ -7,11 +7,9 @@ shinyServer(
       })
     output$zemljevid <- renderPlot({print(zemljevid.drzave)})  
     output$eko.pre <- renderPlot({
-      print(graf.napovedi.preu)
-    })
-    output$eko.eko <- renderPlot({
-      print(graf_napoved.eko)
-    } )
+      print(graf.napovedi.preu)})
+    output$eko.eko <- renderPlot({print(graf_napoved.eko)} )
+    outuput$slika1 <- renderImage({"slike/eko.vinograd.jpg"})
     output$tekst.pre <- renderText({napoved.pre.eko$st.vinogradov[napoved.pre.eko$Leto == "eko.preu"]})
     output$tekst.eko <- renderText({napoved$st.vinogradov[napoved$Leto == "eko.leto"]})
     output$sorte <- DT::renderDataTable(datatable(stidy_sorte.povrsina_sadike))
