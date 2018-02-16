5c9ca9ad33ced81f2244d33508f7b332cb2a310b
mpracek <mpracek299@gmail.com>
2018-02-16 09:09:24 +0100

    zadnji popravki

--- shiny/server.R
+++ shiny/server.R
@@ -7,11 +7,11 @@ shinyServer(
     output$zemljevid <- renderPlot({print(zemljevid.drzave)})  
     output$eko.pre <- renderPlot({print(graf.napovedi.preu)})
     output$eko.eko <- renderPlot({print(graf_napoved.eko)} )
-    output$tekst.pre <- renderText({paste0("V letu ", input$eko.preu, " se predvideva da bo ",
-                                           filter(napoved.pre.eko, Leto == input$eko.preu) %>% select(st.vinogradov),
+    output$tekst.pre <- renderText({paste0("Predvideva se, da bo v letu ",  input$eko.preu, " ",
+                                           round(filter(napoved.pre.eko, Leto == input$eko.preu) %>% select(st.vinogradov), digits = 0),
                                            " vinogradov na kmetijah v preusmeritvi v ekološko kmetijstvo.")})
     output$tekst.eko <- renderText({paste0("Predvideva se, da bo v letu ", input$eko.leto, " ",
-                                           filter(napoved, Leto == input$eko.leto) %>% select(st.vinogradov),
+                                           round(filter(napoved, Leto == input$eko.leto) %>% select(st.vinogradov), digits = 0),
                                            " vinogradov na ekoloških kmetijah")})
     output$sorte <- DT::renderDataTable(datatable(stidy_sorte.povrsina_sadike))
     })
